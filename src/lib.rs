afc44d368d857de3abae054a468498adc9ec1129
Rafael Dias <dias.rafael@gmail.com>
2020-07-31 00:08:12 -0300

    Creating search function

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,23 @@ use std::{fs, error::Error};
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Content to analyze:\n-----------");
-    println!("{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("Result found: {}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.trim().contains(query) {
+            results.push(line.clone());
+        }
+    }
+    results
 }
 
 pub struct Config {
@@ -27,4 +40,34 @@ impl Config {
     }
 }
 
+#[cfg(test)]
+mod tests {
 
+    use super::*;
+
+    #[test]
+    fn it_founds_one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn it_creates_new_config() {
+        let c = Config::new(&vec![
+            "path".to_string(), String::from("query"), String::from("filename")]
+        ).unwrap();
+
+        assert_eq!("query", c.query);
+        assert_eq!("filename", c.filename);
+    }
+
+    #[test]
+    #[should_panic(expected = "Mandatoy arguments not provided: query string and file name.")]
+    fn it_fails_to_create_with_no_args() {
+        let _c: Config = Config::new(&vec!["path".to_string()]).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
+ 
     let config = lib::Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);

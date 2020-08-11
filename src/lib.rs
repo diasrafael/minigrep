e0694e9b39c2abf9bfb456e10e2b80dedfba6ce5
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-08-11 15:15:02 -0300

    fixing 'search_case_insensitive'

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
-        .filter(|line| line.contains(&query.to_lowercase()))
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
 }
 
